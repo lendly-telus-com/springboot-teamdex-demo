38575097dddb18eba595e419045c3d204a38c6a5
Lendly Cagata <lendly.cagata@telus.com>
2023-02-11 00:22:36 +0800

    feat: added openapi

---
+++ teamdex/src/test/java/com/teamdex/teamdex/controller/test/RestUri.java
@@ -0,0 +1,45 @@
+package com.teamdex.teamdex.controller.test;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import com.teamdex.teamdex.controller.DemoController;
+import com.teamdex.teamdex.service.DemoService;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(DemoController.class)
+public class RestUri {
+
+	@MockBean
+	private DemoService service;
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Test
+	public void GetAll() throws Exception {
+		RequestBuilder request = MockMvcRequestBuilders.get("/v1/artists").accept(MediaType.APPLICATION_JSON);
+		mockMvc.perform(request).andExpect(status().isOk()).andReturn();
+
+	}
+//
+//	@Test
+//	public void Post() throws Exception {
+//		RequestBuilder request = MockMvcRequestBuilders.post("/v1/artists").contentType(MediaType.APPLICATION_JSON)
+//				.content().json({"artist_name": "Beatles", "artist_genre": "60's", "albums_recorded": 5, "username": "JohnLennon@yahoo.com" });
+//
+//		mockMvc.perform(request).andExpect(status().isCreated()).andReturn();
+//
+//	}
+
+}
